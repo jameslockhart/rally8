b5f220f62116d038928caa3a389c628abd1ef586
Robin Monks <hi@robinmonks.com>
2014-02-01 13:05:08 -0400

    Adding empty posts.

--- web/index.php
+++ web/index.php
@@ -2,18 +2,34 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+/**********
+ * Includes
+ */
+
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/twigs',
 ));
+
+/*******
+ * Paths
+ */
 
 $app->get('/', function() use ($app) {
   return $app['twig']->render('home.twig', array(
     'auth' => FALSE,
   ));
 });
-$app->get('/hello', function() {
-    return 'Hello!';
+
+$app->post('/login', function() use ($app) {
+    // Do login.
 });
 
+$app->post('/register', function() use ($app) {
+    // Do register.
+});
+
+/************
+ * Do things!
+ */
 $app->run();
