600a0b436c13401dcd4776cb5271ea40dc8cf790
Robin Monks <hi@robinmonks.com>
2014-02-01 12:32:46 -0400

    Homepage maybe

--- web/index.php
+++ web/index.php
@@ -7,6 +7,11 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/twigs',
 ));
 
+$app->get('/', function() use ($app) {
+  return $app['twig']->render('home.twig', array(
+    'auth' => FALSE,
+  ));
+});
 $app->get('/hello', function() {
     return 'Hello!';
 });
