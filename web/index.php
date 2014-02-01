cbc607b21b9d845a34938d390118b794e0faa8e5
Jamie Lockhart <james@hotspotparking.ca>
2014-02-01 15:39:25 -0400

    login stuff

--- web/index.php
+++ web/index.php
@@ -1,10 +1,19 @@
 <?php
 
+/**********
+ * Includes
+ */
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../vendor/Database.php';
 
-/**********
- * Includes
+/*********
+ * Uses
+ */
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/*********
+ * App Setup
  */
 
 $app = new Silex\Application();
@@ -12,9 +21,6 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/twigs',
 ));
 $app['debug'] = true;
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /*******
  * Paths
@@ -26,18 +32,27 @@ $app->get('/', function() use ($app) {
   ));
 });
 
-$app->get('/login', function() use ($app) {
+$app->get('/login', function(Request $request) use ($app) {
     return $app['twig']->render('login.twig', array(
         'auth' => FALSE,
+        'username' => $request->request->get('username')
     ));
 });
 
 $app->post('/login', function(Request $request) use ($app) {
     // Do login. ILL DO IT ~Jamie <3
-    initDatabase($app);
+    init_database($app);
     $sql = "select * from users where username = ? and password = ?";
-    $result = $app['db']->fetchAssoc($sql, array($request->request->get('username'), $request->request->get('password')));
-    return $result;
+    $username = $request->request->get('username');
+    $password = hash_password($request->request->get('password'));
+
+    $result = $app['db']->fetchAssoc($sql, array($username, $password));
+
+    if (!empty($result)) {
+        session_start();
+        $_SESSION['user_id'] = $result['id'];
+        return $app->redirect('/dashboard');
+    } else return false;
 });
 
 $app->get('/signup',function() use($app) {
@@ -45,16 +60,20 @@ $app->get('/signup',function() use($app) {
 });
 
 $app->post('/register', function(Request $request) use ($app) {
-    initDatabase($app);
+    init_database($app);
     // @TODO: Save new user account and automatic log in.
 });
 
 $app->post('/register/check', function(Request $request) use ($app) {
-    initDatabase($app);
+    init_database($app);
     $sql = "select * from users where username = ?";
     $result = $app['db']->fetchRow($sql, array($request->request->get('username')));
 
     return empty($result);
+});
+
+$app->get('/app',function() use($app) {
+    return $app['twig']->render('app.twig');
 });
 
 /************
